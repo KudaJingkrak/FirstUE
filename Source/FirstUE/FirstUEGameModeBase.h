dc06393546e938a60d55844348a27617c5932f47
ArifBudiman19 <arif.budiman41@ui.ac.id>
2019-09-07 20:40:18 +0700

    init c++ blank project

---
+++ Source/FirstUE/FirstUEGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstUEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTUE_API AFirstUEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
